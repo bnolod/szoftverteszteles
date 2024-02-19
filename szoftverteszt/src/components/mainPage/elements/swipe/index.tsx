f7e60b089b355d54045241ca56a8d451f13f9921
bnolod <sadfoolc@gmail.com>
2024-02-19 12:53:42 +0100

    swipe oldal layout

---
+++ szoftverteszt/src/components/mainPage/elements/swipe/index.tsx
@@ -0,0 +1,10 @@
+import {} from 'react';
+
+
+export  function Swipe() {
+    return (
+        <div className="swipe">
+            <h1>hello húzogass</h1>
+        </div>
+    )
+}
--- szoftverteszt/src/components/mainPage/index.tsx
+++ szoftverteszt/src/components/mainPage/index.tsx
@@ -1,11 +1,14 @@
 import {} from "react";
-
+import './index.scss'
+import { Swipe } from './elements/swipe'
 
 
 export  function MainPage() {
     return (
         <div className="mainPage">
-            <h1>MainPage</h1>
+            <div className="main-1"></div>
+            <div className="main-2"><Swipe></Swipe></div>
+            <div className="main-3"></div>
         </div>
     )
 }
