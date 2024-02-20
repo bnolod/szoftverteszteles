098893650f51d2fd032ca238f01dd21bd1f78cba
bnolod <sadfoolc@gmail.com>
2024-02-20 20:33:07 +0100

    login redirect :)

--- szoftverteszt/src/App.tsx
+++ szoftverteszt/src/App.tsx
@@ -5,16 +5,32 @@ import { Register } from './pages/register'
 import {} from 'react'
 import { Routes, Route } from "react-router-dom";
 import { Main } from './pages/mainPage';
+import { auth } from "./firebase"; 
+import {User, onAuthStateChanged} from "firebase/auth"
+import{useEffect, useState} from 'react';
 
 function App() {
 
+  const [authUser, setAuthUser] = useState<User | null>(null);
+  useEffect(() => {
+       onAuthStateChanged(auth,(user) => {
+          if (user) {
+              setAuthUser(user);
+          } else {
+              setAuthUser(null);
+          }
+      });
+  }, [])
   return (
     <>
       <Routes>
         <Route path="welcome" element={<Welcome />}/>
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
-        <Route path="*" element={<Main />} />
+
+      {/* only on auth */}
+        <Route path="*" element={authUser != null ? <Main /> : <Welcome />} />
+        
         
       </Routes>
     </>
